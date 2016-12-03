fd712d591d05ca5df693f9be9708c7cd472ae443
lituan <lituantuan@foxmail.com>
2016-12-04 00:26:05 +0800

    3d

--- mds.py
+++ mds.py
@@ -4,29 +4,67 @@
 use mds to plot high-dimentional data
 """
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn import manifold
 from sklearn.metrics import euclidean_distances
 from sklearn.decomposition import PCA
-
+import os
+import sys
+import scipy.spatial.distance as spd
 import numpy as np
 
-n = 3
-seed = np.random.RandomState(seed=3)
-x=seed.randint(0,20,2*n).astype(np.float)
-x=x.reshape((n,2))
-x-=x.mean()
-similarities=euclidean_distances(x)
-print 'original: ',x
-print 'similarities: ',similarities
+def read_pim(pim_f):
+    with open(pim_f) as o_f:
+        lines = o_f.readlines()
+        lines = [line.rstrip('\n\r') for line in lines] # remove EOF
+        lines = [line for line in lines if ':' in line] # remove comment lines
+        header = [line.split()[1] for line in lines]
+        matrix = [line.split()[2:] for line in lines]
+        matrix = [map(lambda x: float(x)/100.0,row) for row in matrix]
 
-mds=manifold.MDS(n_components=2,max_iter=3000,eps=1e-9,random_state=seed,dissimilarity='precomputed',n_jobs=1)
-pos = mds.fit(similarities).embedding_
-print 'pos: ',pos
-pos *= np.sqrt((x**2).sum())/np.sqrt((pos**2).sum())
-clf=PCA(n_components=2)
-x=clf.fit_transform(x)
+        return header,matrix
 
-fig=plt.figure(1)
-ax=plt.axes([0.,0.,1.,1.])
-plt.scatter(pos[:,0],pos[:,1],s=20,c='g')
-plt.show()
+def plot_mds_3d(labels,matrix,filename):
+
+    scores = np.array(matrix)
+    distances = [map(lambda x: 1.0-x,row) for row in scores]
+    mds=manifold.MDS(n_components=3,max_iter=30000,eps=1e-12,dissimilarity='precomputed',n_jobs=-1)
+    pos = mds.fit(distances)
+    pos = model.embedding_
+    print 'final stress: ' model.stress_
+    # pos_d = euclidean_distances(pos)
+    # print 'pos distances: ', pos_d
+
+    fig=plt.figure()
+    ax = fig.add_subplot(111,projection='3d')
+    ax.scatter(pos[:,0],pos[:,1],pos[:,2],s=20,c='r')
+    plt.savefig(filename+'.png',dpi=300)
+    plt.show()
+    plt.close('all')
+
+def plot_mds(labels,matrix,filename):
+
+    scores = np.array(matrix)
+    distances = [map(lambda x: 1.0-x,row) for row in scores]
+    mds=manifold.MDS(n_components=2,max_iter=30000,eps=1e-12,dissimilarity='precomputed',n_jobs=-1)
+    pos = mds.fit(distances)
+    pos = model.embedding_
+    print 'final stress: ' model.stress_
+    # pos_d = euclidean_distances(pos)
+    # print 'pos distances: ', pos_d
+
+    fig=plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(pos[:,0],pos[:,1],s=20,c='r')
+    plt.savefig(filename+'.png',dpi=300)
+    plt.show()
+    plt.close('all')
+
+def main():
+    header,matrix = read_pim(sys.argv[-1])
+    filename = os.path.splitext(os.path.split(sys.argv[-1])[1])[0]
+    plot_mds(header,matrix,filename)
+
+
+if __name__ == "__main__":
+    main()
